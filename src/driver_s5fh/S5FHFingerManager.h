2126a12916b7a88c2d67719156cc8510924d4e86
Lars Pfotzer <pfotzer@fzi.de>
2014-02-10 12:08:28 +0100

    Added enum for finger dof mapping.

--- src/driver_s5fh/S5FHFingerManager.h
+++ src/driver_s5fh/S5FHFingerManager.h
@@ -23,12 +23,24 @@ namespace driver_s5fh {
 class DRIVER_S5FH_IMPORT_EXPORT S5FHFingerManager
 {
 public:
+
   /*! Constructs a finger manager for the SCHUNK five finger hand.
    */
   S5FHFingerManager();
 
   ~S5FHFingerManager();
 
+  enum {
+    eS5FH_THUMB_FLEXION,
+    eS5FH_THUMB_OPPOSITION, // wrist
+    eS5FH_INDEX_FINGER_DISTAL,
+    eS5FH_INDEX_FINGER_PROXIMAL,
+    eS5FH_MIDDLE_FINGER_DISTAL,
+    eS5FH_MIDDLE_FINGER_PROXIMAL,
+    eS5FH_RING_FINGER,
+    eS5FH_PINKY,
+    eS5FH_FINGER_SPREAD
+  } typedef S5FHDOF;
 };
 
 }

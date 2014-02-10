0b727a32aa32ead03f19416a64a734e54977749c
Georg Heppner <heppner@fzi.de>
2014-02-10 23:01:18 +0100

    Removed unneccessary debug outputs in tests, added tests

--- src/ts/driver_s5fh/ts_S5FHDriver.cpp
+++ src/ts/driver_s5fh/ts_S5FHDriver.cpp
@@ -23,51 +23,28 @@ using namespace driver_s5fh;
 
 BOOST_AUTO_TEST_SUITE(ts_S5FHDriver)
 
-S5FHPositionSettings test_pos_settings =  {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.1};
-S5FHCurrentSettings test_cur_settings =  {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.1};
-
-std::vector<uint8_t> test_vector;
-std::vector<uint16_t> test_vector2;
-
 BOOST_AUTO_TEST_CASE(ConvertSettingsTest)
 {
+  S5FHPositionSettings test_pos_settings =  {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.1};
+
   // See if the conversion of position settings worked
   ArrayBuilder ab(40);
-  ArrayBuilder ab2(40);
   ab << test_pos_settings;
-  ab2 << test_cur_settings;
   std::cout << "S5FH Driver array builder test:" << std::endl;
   std::cout << "  Position Settings raw data: " << ab << std::endl;
-  std::cout << "------------------------------" << std::endl;
-  std::cout << "  Current Settings raw data: " << ab2 << std::endl;
+  std::cout << "=================================================="<<std::endl<<std::endl;
 
-  // Test if the Vectors Work with the ArrayBuilder
-  test_vector.push_back(1);
-  test_vector.push_back(2);
-  test_vector.push_back(3);
-  test_vector.push_back(4);
-  ab << test_vector ;
-
-  std::cout << "Testvector " << ab << std::endl;
-
-  test_vector2.push_back(5);
-  test_vector2.push_back(6);
-  test_vector2.push_back(7);
-  test_vector2.push_back(8);
-  ab << test_vector2 ;
-
-  std::cout << "Testvector2 " << ab << std::endl;
-
-  // TODO: Make these to automated tests ;) and not just couts
 }
 
 BOOST_AUTO_TEST_CASE(ConvertSerialPacket)
 {
-  std::cout << std::endl;
+  std::cout << "Testing Conversion of Serial Packet" << std::endl;
+
 
   ArrayBuilder payload(40);
   ArrayBuilder packet;
   S5FHSerialPacket test_serial_packet;
+  S5FHPositionSettings test_pos_settings =  {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.1};
 
   // Get the Position settings as payload
   payload << test_pos_settings;
@@ -80,8 +57,15 @@ BOOST_AUTO_TEST_CASE(ConvertSerialPacket)
   // serialize the Packet
   packet << test_serial_packet;
 
-  std::cout << "The Serial packet with the adress: " << (int)test_serial_packet.address << " index: " << (int)test_serial_packet.index << "Length: " << test_serial_packet.data.size() << " Has the following RAW data: "<< std::endl;
-  std::cout << "Packet: " << packet << std::endl;
+  //std::cout << "Serial packet after transformation into byte array: " << test_serial_packet;
+
+  // Create the structure to read in the packet
+  S5FHSerialPacket test_serial_packet_in(40);
+  // De-Serialize
+  packet >> test_serial_packet_in;
+
+  BOOST_CHECK_EQUAL(test_serial_packet,test_serial_packet_in);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()

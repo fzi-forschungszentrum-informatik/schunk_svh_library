60b32c8b43db6d4c51a7c86503b0089c41d4ace9
Lars Pfotzer <pfotzer@fzi.de>
2014-02-10 12:21:56 +0100

    Added functions for stetting controller parameters and target position.

--- src/driver_s5fh/S5FHFingerManager.cpp
+++ src/driver_s5fh/S5FHFingerManager.cpp
@@ -21,10 +21,50 @@ namespace driver_s5fh {
 S5FHFingerManager::S5FHFingerManager()
 {
   m_controller = new S5FHController();
+
+  // 1. load current and position setting from config file
+  // 2. set controller with current and position setting
+
 }
 
 S5FHFingerManager::~S5FHFingerManager()
 {
+}
+
+//! reset function for a single finger
+bool S5FHFingerManager::resetFinger(const S5FHDOF &index)
+{
+  return true;
+}
+
+//! set target position of a single finger
+bool S5FHFingerManager::setTargetPosition(const S5FHDOF &index, double position, double current)
+{
+  S5FHControlCommand position_command;
+  position_command.position = position;
+
+  m_controller->setControllerTarget(index, position_command);
+  return true;
+}
+
+//! overwrite current parameters
+bool S5FHFingerManager::setCurrentControllerParams(const S5FHDOF &index)
+{
+  S5FHCurrentSettings current_settings;
+  // TODO: set parameters
+
+  m_controller->setCurrentSettings(index, current_settings);
+  return true;
+}
+
+//! overwrite position parameters
+bool S5FHFingerManager::setPositionControllerParams(const S5FHDOF &index)
+{
+  S5FHPositionSettings position_settings;
+  // TODO: set parameters
+
+  m_controller->setPositionSettings(index, position_settings);
+  return true;
 }
 
 }
--- src/driver_s5fh/S5FHFingerManager.h
+++ src/driver_s5fh/S5FHFingerManager.h
@@ -43,6 +43,20 @@ public:
     eS5FH_FINGER_SPREAD
   } typedef S5FHDOF;
 
+
+  //! reset function for a single finger
+  bool resetFinger(const S5FHDOF &index);
+
+  //! set target position of a single finger
+  bool setTargetPosition(const S5FHDOF &index, double position, double current);
+
+  //! overwrite current parameters
+  bool setCurrentControllerParams(const S5FHDOF &index);
+
+  //! overwrite position parameters
+  bool setPositionControllerParams(const S5FHDOF &index);
+
+
 private:
 
   //! pointer to s5fh controller

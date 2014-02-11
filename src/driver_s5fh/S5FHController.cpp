4b48912a2d4d63646a5af037a40a86ab89c4ab10
Georg Heppner <heppner@fzi.de>
2014-02-11 15:11:53 +0100

    Implemented function stubs for controller

--- src/driver_s5fh/S5FHController.cpp
+++ src/driver_s5fh/S5FHController.cpp
@@ -25,9 +25,78 @@ S5FHController::~S5FHController()
 {
 }
 
-void S5FHController::setControllerTarget(size_t index, const S5FHControlCommand& target_position)
+
+void S5FHController::setControllerTarget(const size_t& channel, const u_int32_t& position)
+{
+
+
+}
+
+void S5FHController::enableChannel(const size_t &channel)
 {
 
 }
+
+
+void S5FHController::disableChannel(const size_t& channel)
+{
+
+}
+
+
+void S5FHController::getControllerFeedback(const size_t& channel)
+{
+
+}
+
+
+void S5FHController::getPositionSettings(const size_t& channel)
+{
+
+}
+
+
+void S5FHController::setPositionSettings(const size_t& channel,const S5FHPositionSettings& position_settings)
+{
+   //m_position_settings[channel] = position_settings;
+}
+
+
+void S5FHController::getCurrentSettings(const size_t& channel)
+{
+
+}
+
+
+void S5FHController::setCurrentSettings(const size_t& channel,const S5FHCurrentSettings& current_settings)
+{
+     // m_current_settings[channel] = current_settings;
+}
+
+
+void S5FHController::getEncoderValues()
+{
+
+}
+
+//TODO: EncoderValues Type
+//void setEncoderValues
+
+void S5FHController::getFirmwareInfo()
+{
+
+}
+
+
+ReceivedPacketCallback S5FHController::receivedPacketCallback(const S5FHSerialPacket& packet, unsigned int packet_count)
+{
+
+}
+
+
+
+
+
+
 
 }
--- src/driver_s5fh/S5FHController.h
+++ src/driver_s5fh/S5FHController.h
@@ -15,13 +15,15 @@
 #ifndef DRIVER_S5FH_S5FH_CONTROLLER_H_INCLUDED
 #define DRIVER_S5FH_S5FH_CONTROLLER_H_INCLUDED
 
-#include "driver_s5fh/ImportExport.h"
-#include "driver_s5fh/S5FHSerialInterface.h"
-#include "driver_s5fh/S5FHControlCommand.h"
-#include "driver_s5fh/S5FHControllerFeedback.h"
-#include "driver_s5fh/S5FHCurrentSettings.h"
-#include "driver_s5fh/S5FHFirmwareInfo.h"
-#include "driver_s5fh/S5FHPositionSettings.h"
+#include <driver_s5fh/ImportExport.h>
+#include <driver_s5fh/S5FHSerialInterface.h>
+#include <driver_s5fh/S5FHReceiveThread.h>
+#include <driver_s5fh/S5FHControlCommand.h>
+#include <driver_s5fh/S5FHControllerFeedback.h>
+#include <driver_s5fh/S5FHCurrentSettings.h>
+#include <driver_s5fh/S5FHFirmwareInfo.h>
+#include <driver_s5fh/S5FHPositionSettings.h>
+#include <driver_s5fh/S5FHControllerState.h>
 
 namespace driver_s5fh {
 
@@ -34,30 +36,109 @@ public:
    */
   S5FHController();
 
+  /*! SCHUNK five finger hand destructor
+   *  Disable the serial device and shut down hand as far
+   *  as possible
+   */
   ~S5FHController();
 
-  //! overwrite current controller parameters of finger index
-  void setCurrentSettings(size_t index, const S5FHCurrentSettings& current_settings)
-  {
-    m_current_settings[index] = current_settings;
-  }
+  /*!
+   * \brief setting new position controller target for finger index
+   * \param channel Motorchanel to set the target for
+   * \param position Target position given in encoder Ticks
+   */
+  void setControllerTarget(const size_t& channel, const u_int32_t& position);
 
-  //! overwrite position controller parameters of finger index
-  void setPositionSettings(size_t index, const S5FHPositionSettings& position_settings)
-  {
-    m_position_settings[index] = position_settings;
-  }
 
-  //! setting new position controller target for finger index
-  void setControllerTarget(size_t index, const S5FHControlCommand& target_position);
+  // Access functions
+  //!
+  //! \brief Enable one or all motor channels
+  //! \param channel Motor to activate 0 for all
+  //!
+  void enableChannel(const size_t& channel);
+
+  //!
+  //! \brief Disable one or all motor channels
+  //! \param channel Motor to deactivate 0 for all
+  //!
+  void disableChannel(const size_t& channel);
+
+  //!
+  //! \brief request feedback (position and current) to a specific channel
+  //! \param channel Motorchannel the feedback should be provided for
+  //!
+  void getControllerFeedback(const size_t& channel);
+
+  /*!
+   * \brief request the settings of the position controller for a specific channel
+   * \param channel Motor to request the settings for
+   */
+  void getPositionSettings(const size_t& channel);
+
+  /*!
+   * \brief activate a new set of position controller settings for a specific channel
+   * \param channel Motor the new position controller settings will be applied to
+   * \param position_settings new settings of the position controller
+   */
+  void setPositionSettings(const size_t& channel,const S5FHPositionSettings& position_settings);
+
+  /*!
+   * \brief request the settings of the current controller for a specific channel
+   * \param channel Motor to request the settings for
+   */
+  void getCurrentSettings(const size_t& channel);
+
+  /*!
+   * \brief activate a new set of current controller settings for a specific channel
+   * \param channel Motor the new current controller settings will be applied to
+   * \param current_settings new settings of the current controller
+   */
+  void setCurrentSettings(const size_t& channel,const S5FHCurrentSettings& current_settings);
+
+  /*!
+   * \brief read out the mutipliers for the encoders
+   */
+  void getEncoderValues();
+
+  //TODO: EncoderValues Type
+  //void setEncoderValues
+
+  /*!
+   * \brief request a transmission of formware information
+   */
+  void getFirmwareInfo();
+
+
+  ReceivedPacketCallback receivedPacketCallback(const S5FHSerialPacket& packet, unsigned int packet_count);
+
+
+ //! definition of boost function callback for received packages
+ //typedef boost::function<void (const S5FHSerialPacket& packet, unsigned int packet_count)> ReceivedPacketCallback;
 
 private:
+
+  // Data Structures for holding configurations and feedback of the Controller
 
   //! vector of current controller parameters for each finger
   std::vector<S5FHCurrentSettings> m_current_settings;
 
   //! vector of position controller parameters for each finger
   std::vector<S5FHPositionSettings> m_position_settings;
+
+  //! ControllerFeedback indicates current position and current per finger
+  std::vector<S5FHControllerFeedback> m_controller_feedback;
+
+  //! Currently active controllerstate on the HW Controller (indicates if PWM active etc.)
+  S5FHControllerState m_controller_state;
+
+
+  // Hardware control
+
+  //! Serial interface for transmission and reveibing of data packets
+  //S5FHSerialInterface m_serial_interface;
+
+
+
 
 };
 

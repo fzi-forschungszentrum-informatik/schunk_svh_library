b3d1159bd06691d1a1f3440d3f9db6794e0cc0d0
Georg Heppner <heppner@fzi.de>
2014-02-12 19:23:56 +0100

    Tried to make the firmwareInfo better.. but ..well it does not work.. we can fix that later

--- src/driver_s5fh/S5FHFirmwareInfo.h
+++ src/driver_s5fh/S5FHFirmwareInfo.h
@@ -19,21 +19,20 @@
 
 namespace driver_s5fh {
 
-//TODO: Use strings? i dont know. what is acutally in these data
 
 /*!
  * \brief The S5FHFirmwareInfo  holds the data of a firmware response from the hardware
  */
 struct S5FHFirmwareInfo
 {
-  //! 4 Char identifier
-  uint8_t s5fh[4];
+  //! 4 bytes identifier
+  std::string s5fh;
   //! Major version number
   uint16_t version_major;
   //! Minor version number
   uint16_t version_minor;
-  //! 48 chars of text (free)
-  uint8_t text[48];
+  //! 48 bytes! of text (free)
+  std::string text;
 
   //! Compares two S5FHFirmware objects.
   bool operator == (const S5FHFirmwareInfo& other) const
@@ -43,8 +42,26 @@ struct S5FHFirmwareInfo
        && version_minor == other.version_minor
        );
   }
-
 };
+
+//! overload stream operator to easily serialize data
+inline icl_comm::ArrayBuilder& operator << (icl_comm::ArrayBuilder& ab, S5FHFirmwareInfo& data)
+{
+  // Stream operator can not handle arrays (due to missing size information) to make things easy we just copy the data around. Feel free to do something else
+  // Todo : Can the operator handle this without conversion? Also .. string uses char.. and not unsigned char.. but that is also 8 bit so.. we are save.. maybe we have to use std::basic_string<unsinge_char> ... try it..
+  std::vector<uint8_t> text(48);
+  std::vector<uint8_t> s5fh(4);
+  s5fh.insert(s5fh.begin(),data.s5fh.begin(),data.s5fh.end());
+  text.insert(text.begin(),data.text.begin(),data.text.end());
+
+  ab << s5fh
+     << data.version_major
+     << data.version_minor
+     << text;
+  return ab;
+}
+
+
 
 //! overload stream operator to easily serialize data
 inline icl_comm::ArrayBuilder& operator >> (icl_comm::ArrayBuilder& ab, S5FHFirmwareInfo& data)
@@ -58,11 +75,23 @@ inline icl_comm::ArrayBuilder& operator >> (icl_comm::ArrayBuilder& ab, S5FHFirm
      >> data.version_major
      >> s5fh;
 
-  std::copy (text.begin(),text.end(),data.text);
-  std::copy (s5fh.begin(),s5fh.end(),data.s5fh);
+
+  data.text = std::string(text.begin(),text.end());
+  data.s5fh = std::string(s5fh.begin(),s5fh.end());
+//  std::copy (text.begin(),text.end(),data.text);
+//  std::copy (s5fh.begin(),s5fh.end(),data.s5fh);
 
   return ab;
 }
+
+//! Output Stream operator
+inline std::ostream& operator << (std::ostream& o, const S5FHFirmwareInfo& fw)
+{
+  o << fw.s5fh.c_str()  << " " << fw.version_major << "." << fw.version_minor << " : " << fw.text.c_str() << endl;
+  return o;
+}
+
+
 
 }
 

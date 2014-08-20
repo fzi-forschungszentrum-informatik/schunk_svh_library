8b62bdcfeccb384a41d350f1cb7756ede949d135
Georg Heppner <heppner@fzi.de>
2014-08-20 11:08:30 +0200

    Added Commented out testcase to reproduce the start of connection as console output

--- src/ts/driver_s5fh/ts_S5FHDriver.cpp
+++ src/ts/driver_s5fh/ts_S5FHDriver.cpp
@@ -22,6 +22,7 @@
 #include <driver_s5fh/S5FHControllerFeedback.h>
 #include <driver_s5fh/S5FHEncoderSettings.h>
 #include <driver_s5fh/S5FHController.h>
+#include <driver_s5fh/S5FHFingerManager.h>
 
 using icl_comm::ArrayBuilder;
 using namespace driver_s5fh;
@@ -225,6 +226,9 @@ BOOST_AUTO_TEST_CASE(ConvertSerialPacket)
   test_serial_packet_in.data = payload.array;
   // Converstion (this would be done by serialinterface)
   packet << test_serial_packet_in;
+
+ // std::cout << "Raw Serial Packet of Position settings: " << packet << std::endl;
+
   packet >> test_serial_packet_out;
   payload.reset(40);
   payload.appendWithoutConversion(test_serial_packet_out.data);
@@ -233,7 +237,7 @@ BOOST_AUTO_TEST_CASE(ConvertSerialPacket)
   BOOST_CHECK_EQUAL(test_serial_packet_in,test_serial_packet_out);
   BOOST_CHECK_EQUAL(test_pos_settings_in,test_pos_settings_out);
 
- // std::cout << "Original pos Settings: " << test_pos_settings_in << " Interpreted pos settings: "<< test_pos_settings_out <<std::endl;
+  //std::cout << "Original pos Settings: " << test_pos_settings_in << " Interpreted pos settings: "<< test_pos_settings_out <<std::endl;
   std::cout << "Done" << std::endl;
 }
 
@@ -273,6 +277,74 @@ BOOST_AUTO_TEST_CASE(ControllerreceiveFeedback)
 
   std::cout << "Done" << std::endl;
 }
+
+// This should not be an auto Testcase but rather one to execute voluntarily -> read about how to do that
+//BOOST_AUTO_TEST_CASE(InitialConnectionPackets)
+//{
+//  std::cout << "Printing out raw Data that should be sent for a first transmission of everything:...." << std::endl;
+
+//  // Initialize logging
+//  icl_core::logging::initialize();
+
+//  std::cout << "Deactivating all the channels..................." << std::endl;
+
+//  // First packet -> Disable all the channels
+//  S5FHSerialPacket serial_packet(0,S5FH_SET_CONTROLLER_STATE);
+//  S5FHControllerState controller_state;
+//  S5FHSerialInterface serial_interface(NULL);
+//  S5FHFingerManager finger_manager;
+//  ArrayBuilder ab(40);
+
+//  // load default position settings before the fingers are resetted
+//  std::vector<S5FHPositionSettings> default_position_settings
+//      = finger_manager.getPositionSettingsDefaultResetParameters();
+
+//  // load default current settings
+//  std::vector<S5FHCurrentSettings> default_current_settings
+//      = finger_manager.getCurrentSettingsDefaultParameters();
+
+
+//  controller_state.pwm_fault = 0x001F;
+//  controller_state.pwm_otw   = 0x001F;
+
+//  // default initialization to zero -> controllers are deactivated
+//  ab << controller_state;
+//  serial_packet.data = ab.array;
+//  serial_interface.printPacketOnConsole(serial_packet);
+
+//  // initialize all channels
+//  for (size_t i = 0; i < eS5FH_DIMENSION; ++i)
+//  {
+//    std::cout << "Requesting feedback for channel : "<< i << " ........." << std::endl;
+//    // request controller feedback
+//    S5FHSerialPacket serial_packet1(40,S5FH_GET_CONTROL_FEEDBACK|static_cast<u_int8_t>(static_cast<S5FHCHANNEL>(i) << 4));
+//    serial_interface.printPacketOnConsole(serial_packet1);
+
+
+//    std::cout << "Transmitting position settings for channel : "<< i << " ........." << std::endl;
+
+//    // set position settings
+//    S5FHSerialPacket serial_packet2(0,S5FH_SET_POSITION_SETTINGS|static_cast<u_int8_t>(static_cast<S5FHCHANNEL>(i) << 4));
+//    ArrayBuilder ab2;
+//    ab2 << default_position_settings[i];
+//    serial_packet2.data = ab2.array;
+//    serial_interface.printPacketOnConsole(serial_packet2);
+
+//    std::cout << "Transmitting current settings for channel : "<< i << " ........." << std::endl;
+
+//    // set current settings
+//    S5FHSerialPacket serial_packet3(0,S5FH_SET_CURRENT_SETTINGS|static_cast<u_int8_t>(static_cast<S5FHCHANNEL>(i) << 4));
+//    ArrayBuilder ab3;
+//    ab3 << default_current_settings[i];
+//    serial_packet3.data = ab3.array;
+//    serial_interface.printPacketOnConsole(serial_packet3);
+//  }
+
+
+//  std::cout << "Done" << std::endl;
+
+
+//}
 
 
 // Firmware is not working yet

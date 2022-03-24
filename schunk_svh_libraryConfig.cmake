25f43eb25eae09f3a467fd2c2165328ca09f0fda
Felix Exner <exner@fzi.de>
2022-03-24 21:55:34 +0100

    Export LIBRARIES and INCLUDE_DIRS
    This is important for legacy cmake systems such as catkin

--- schunk_svh_libraryConfig.cmake
+++ schunk_svh_libraryConfig.cmake
@@ -1,3 +1,8 @@
 include(CMakeFindDependencyMacro)
 #find_dependency(some-other-package)
+
 include("${CMAKE_CURRENT_LIST_DIR}/schunk_svh_libraryTargets.cmake")
+
+# This is for catkin compatibility. Better use target_link_libraries(<my_target> schunk_svh_library::svh-library)
+set(schunk_svh_library_LIBRARIES schunk_svh_library::svh-library)
+get_target_property(schunk_svh_library_INCLUDE_DIRS schunk_svh_library::schunk_svh_library INTERFACE_INCLUDE_DIRECTORIES)

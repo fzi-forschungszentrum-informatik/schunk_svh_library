470e0711b88f9b880c04e8b4705e6c138009f88d
Felix Exner <exner@fzi.de>
2022-03-24 22:15:00 +0100

    Fixed library exports

--- schunk_svh_libraryConfig.cmake
+++ schunk_svh_libraryConfig.cmake
@@ -3,6 +3,6 @@ include(CMakeFindDependencyMacro)
 
 include("${CMAKE_CURRENT_LIST_DIR}/schunk_svh_libraryTargets.cmake")
 
-# This is for catkin compatibility. Better use target_link_libraries(<my_target> schunk_svh_library::svh-library)
-set(schunk_svh_library_LIBRARIES schunk_svh_library::svh-library)
-get_target_property(schunk_svh_library_INCLUDE_DIRS schunk_svh_library::schunk_svh_library INTERFACE_INCLUDE_DIRECTORIES)
+# This is for catkin compatibility. Better use target_link_libraries(<my_target> Schunk::svh-library)
+set(schunk_svh_library_LIBRARIES Schunk::svh-library)
+get_target_property(schunk_svh_library_INCLUDE_DIRS Schunk::svh-library INTERFACE_INCLUDE_DIRECTORIES)
